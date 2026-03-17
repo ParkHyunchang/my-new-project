bf29275f8d376d88d53e487639f2ca13608e5cbc
박현창 <hyunchang@ponylink.co.kr>
2026-03-17 17:22:39 +0900

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #ffffff;
+}
+</style>
